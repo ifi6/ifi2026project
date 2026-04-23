914ce1aa199c8b6b1bf0a1113c4f6aeb50a8059c
SaZahed <szahed@stud.hs-bremen.de>
2026-04-23 13:25:34 +0200

    Kommentare ergänzt

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,10 +1,16 @@
-var builder = WebApplication.CreateBuilder(args);
+/*
+ * dependencies problem
+ * file wird isoliert ausgefuehrt und findet webapplication nicht
+ * ueber terminal ins API Verzeichnis und mit dotnet run ausfuehren --> so wird die csproj beachtet
+ * 
+ */
+var builder = WebApplication.CreateBuilder(args); 
 
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddControllers(); // um controller zu verwenden
+builder.Services.AddEndpointsApiExplorer(); //endpoints fuer swagger
+builder.Services.AddSwaggerGen(); //swagger aktivieren 
 
-var app = builder.Build();
+var app = builder.Build(); //anwendung bauen 
 
 if (app.Environment.IsDevelopment())
 {
@@ -12,8 +18,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+app.UseHttpsRedirection(); //https umleitung 
 
-app.MapControllers();
+app.MapControllers(); //macht alle controller erreichbar 
 app.MapGet("/", () => "API läuft");
 app.Run();
